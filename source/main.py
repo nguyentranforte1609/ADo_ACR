e78c8e0274b6244f43ab12ba2aa7e115faf6e40d
Nguyen Tran <nguyen.tran@idealogic.com.vn>
2019-07-16 15:08:01 +0700

    Add .gitignore & Complete code for capturing and replaying keyboard events and mouse left click

---
+++ source/main.py
@@ -0,0 +1,76 @@
+from pynput import mouse, keyboard
+import pyautogui
+
+class ADo:
+    def __init__(self):
+        self.events = []
+        self.enableCaptureEvents = False
+        self.stopHotkey = keyboard.Key.esc
+        self.replayHotkey = keyboard.KeyCode.from_char('`')
+
+    def captureEvents(self):
+        self.enableCaptureEvents = True
+        print('Start capturing events: ')
+        with mouse.Listener(
+            on_click = self.callBackOnClick
+        ) as ML, keyboard.Listener(
+            on_press = self.callBackOnPress
+        ) as KL:
+            KL.join()
+            ML.join()
+        return self.events
+
+    def waitForInput(self):
+        print('Waiting for hotkey input...')
+        print('Press "`" to replay. Press ESC to terminate.')
+        with keyboard.Listener(
+            on_press = self.callBackHotKeyPress
+        ) as KL:
+            KL.join()
+
+    def callBackOnClick(self, x, y, button, pressed):
+        if self.enableCaptureEvents:
+            if pressed:
+                print('Capture click event at {}'.format((x,y)))
+                self.events.append((x,y))
+        else:
+            return False
+
+    def callBackOnPress(self, key):
+        if self.enableCaptureEvents:        
+            if key == self.stopHotkey:
+                print('Stop capturing event!!!')
+                self.enableCaptureEvents = False
+                mouse.Controller().click(mouse.Button.left)
+                return False
+            else:
+                print('Capture key press {}'.format(key))
+                self.events.append(key)        
+
+    def callBackHotKeyPress(self, key):
+        if key == self.replayHotkey:
+            print('Replay events!!!')
+            self.replayEvents()
+        if key == self.stopHotkey:
+            print('Stop program!!!')
+            return False
+
+    def replayEvents(self):
+    
+        keyboardCtrl = keyboard.Controller()
+        for event in self.events:
+            if type(event) is tuple:
+                previousPosition = pyautogui.position()
+                pyautogui.move(event[0] - previousPosition[0], event[1] - previousPosition[1])
+                pyautogui.click(button='left');
+            else:
+                keyboardCtrl.press(event)
+                keyboardCtrl.release(event)
+                
+"""
+"" App Start
+"""
+if __name__ == "__main__":
+    app = ADo()
+    app.captureEvents()
+    app.waitForInput()
