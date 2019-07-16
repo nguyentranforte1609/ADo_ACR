ee015071ea05cd8cc5a893ef6e3755e4399912ed
Nguyen Tran <nguyen.tran@idealogic.com.vn>
2019-07-16 16:49:04 +0700

    Now waiting until start hotkey is pressed then start capturing events. Refactor code a bit

--- source/main.py
+++ source/main.py
@@ -5,8 +5,13 @@ class ADo:
     def __init__(self, replayHotkey):
         self.events = []
         self.enableCaptureEvents = False
+        self.startHotkey = keyboard.Key.enter
         self.stopHotkey = keyboard.Key.esc
         self.replayHotkey = keyboard.KeyCode.from_char(replayHotkey)
+
+    def run(self):
+        self.waitForCaptureCommand()
+        self.waitForReplayCommand()    
 
     def captureEvents(self):
         self.enableCaptureEvents = True
@@ -20,13 +25,37 @@ class ADo:
             ML.join()
         return self.events
 
-    def waitForInput(self):
+    def replayEvents(self):   
+        mouseCtrl = mouse.Controller()
+        keyboardCtrl = keyboard.Controller()
+        for event in self.events:
+            if type(event) is tuple:
+                previousPosition = pyautogui.position()
+                pyautogui.move(event[0] - previousPosition[0], event[1] - previousPosition[1])
+                mouseCtrl.click(event[2])
+            else:
+                keyboardCtrl.press(event)
+                keyboardCtrl.release(event)
+
+    def waitForCaptureCommand(self):
+        print('Press Enter to start capturing events...')
+        with keyboard.Listener(
+            on_press = self.callBackStartCapture
+        ) as KL:
+            KL.join()
+
+    def waitForReplayCommand(self):
         print('Waiting for hotkey input...')
         print('Press ` to replay. Press ESC to terminate.')
         with keyboard.Listener(
             on_press = self.callBackHotKeyPress
         ) as KL:
             KL.join()
+    
+    def callBackStartCapture(self, key):
+        if key == self.startHotkey:
+            self.captureEvents()
+            return False
 
     def callBackOnClick(self, x, y, button, pressed):
         if self.enableCaptureEvents:
@@ -55,30 +84,20 @@ class ADo:
             print('Stop program!!!')
             return False
 
-    def replayEvents(self):   
-        mouseCtrl = mouse.Controller()
-        keyboardCtrl = keyboard.Controller()
-        for event in self.events:
-            if type(event) is tuple:
-                previousPosition = pyautogui.position()
-                pyautogui.move(event[0] - previousPosition[0], event[1] - previousPosition[1])
-                mouseCtrl.click(event[2])
-            else:
-                keyboardCtrl.press(event)
-                keyboardCtrl.release(event)
-                
+
+
 """
     App Start
 """
 if __name__ == "__main__":
     replayHotkey = '`'
     app = ADo(replayHotkey)
-    app.captureEvents()
-    app.waitForInput()
+    app.run()
     """
         I use character ` as default hotkey for replay events.
         Feel free to change it as your desired key by replacing the value of "replayHotkey" variable in __main__
         In case you want to use functional key such as F1, Ctrl, Shift, etc. then you should use the pre-defined keys in pynput.keyboard.Key.<your desired key>
 
+        ENTER key is the default key to start capturing events.
         ESC key is the default key for terminating program.
     """
